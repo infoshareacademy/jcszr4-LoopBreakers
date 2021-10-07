dabc5fb47541460da888485a2a7dd11e5c7de73d
RafalSzczerba <rafalszczerbapk@gmail.com>
2021-10-07 16:05:18 +0200

    modyfikacja wczytywania danych oraz utworzenie fukcji wyszukiwania po nazwie

--- LoopBreakers.ConsoleApp/Program.cs
+++ LoopBreakers.ConsoleApp/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using LoopBreakers.Logic.Data;
+using System.Collections;
+using System.Collections.Generic;
+using LoopBreakers.Logic.Static;
+using System.Linq;
 
 namespace LoopBreakers.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TemporaryCollections.InitializeCollections();
+
+
+
+            //Option 1 - SearchByName
+            int chosenOption = 1;   //should be comment
+            if (chosenOption == 1)
+            {
+                
+                Console.WriteLine("Introduce the surname of user which you wish to find");
+                var entryName1 = Console.ReadLine();
+                if (TemporaryCollections.Users.Any())
+                {
+                    Search.NameSearch(entryName1);
+                }
+                else
+                {
+                    Console.WriteLine("No users in the scope");
+                }
+
+            }
         }
     }
 }
