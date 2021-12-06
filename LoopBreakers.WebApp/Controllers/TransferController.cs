0616a1d77575894edf982baec1cc5c1bd05988d1
Małgorzata Łukasik <malgorzata.lukasik1@gmail.com>
2021-12-06 22:53:57 +0100

    lista sie wyświetla

---
+++ LoopBreakers.WebApp/Controllers/TransferController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoopBreakers.WebApp.Contracts;
+using LoopBreakers.WebApp.Data;
+
+namespace LoopBreakers.WebApp.Controllers
+{
+    public class TransferController : Controller
+    {
+        // GET: TransferController
+        private readonly ITransfersRepository _transfersRepository;
+        public TransferController(ITransfersRepository transfersRepository)
+        {
+            _transfersRepository = transfersRepository;
+        }
+        public ActionResult Index()
+        {
+            var model = _transfersRepository.FindAll().Result;
+            return View(model);
+        }
+
+        // GET: TransferController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TransferController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TransferController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult FilteredByDate()
+        {
+            
+            return View();
+        }
+
+        // POST: TransferController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult FilteredByDate(DateTime dateValue)
+        {
+            var x = dateValue;
+            var model = _transfersRepository.FindAll().Result;
+            var xx = model.Where(m => m.Created < dateValue);
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TransferController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TransferController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TransferController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TransferController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
