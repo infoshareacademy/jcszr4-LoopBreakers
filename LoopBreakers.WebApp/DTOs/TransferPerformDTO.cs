095b11bd720090ff9f3f75610f4b3299e486fa9c
Bartłomej Zieliński <bartekz@gmail.com>
2022-01-23 14:57:57 +0100

    poprawki widoku transferu

--- LoopBreakers.WebApp/DTOs/TransferPerformDTO.cs
+++ LoopBreakers.WebApp/DTOs/TransferPerformDTO.cs
@@ -6,24 +6,32 @@ namespace LoopBreakers.WebApp.DTOs
 {
     public class TransferPerformDTO
     {
-        [Display(Name ="User Suranme")]
+        [Display(Name = "Client Surname")]
         [Required]
         public string UserSurname { get; set; }
+
         [Required]
         [MinLength(28)]
         [MaxLength(28)]
         public string Iban { get; set; }
 
+        [Required]
         [Display(Name = "Reciever First Name")]
         public string FirstName { get; set; }
+
+        [Required]
         [Display(Name = "Reciever Last Name")]
         public string LastName { get; set; }
         public string FromId { get; set; }
         public TransferType Type { get; set; }
-        [Range(0.01, 20000, ErrorMessage = "Kwota musi byc w zakresie od 0.01 do 20000")]
+
+        [Required]
+        [Range(0.01, 20000, ErrorMessage = "The amount must be betwen 0.01 and 20000")]
         public decimal Amount { get; set; }
 
         public DateTime Created { get; set; }
+
+        [Required]
         public string Reference { get; set; }
         public Currency Currency { get; set; }
     }
