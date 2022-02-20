289f3fb088e2c5f4d2af522678b19848e1bc7a66
Bartłomej Zieliński <bartekz@gmail.com>
2022-02-20 19:59:40 +0100

    init report module

---
+++ LoopBreakers.ReportModule/Controllers/ActivityReportController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoopBreakers.ReportModule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityReportController : ControllerBase
+    {
+        // GET: api/<ActivityReportController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ActivityReportController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ActivityReportController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ActivityReportController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ActivityReportController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
