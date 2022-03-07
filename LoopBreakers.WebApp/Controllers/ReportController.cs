72b6cd235f9d1a6d1b4ba6fb57527ffcd1ca568b
Małgorzata Łukasik <malgorzata.lukasik1@gmail.com>
2022-03-07 23:00:01 +0100

    kontroler i widok

---
+++ LoopBreakers.WebApp/Controllers/ReportController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoopBreakers.DAL.Enums;
+using LoopBreakers.ReportModule.Models;
+using LoopBreakers.ReportModule.Services;
+
+namespace LoopBreakers.WebApp.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+            
+        }
+
+
+        // GET: ReportController
+        public ActionResult Index()
+        {
+            var model = new List<TransferReportDTO>();
+            var model1 = new TransferReportDTO();
+            model1.Amount = 10;
+            model1.Created = DateTime.Now;
+            model1.Currency = Currency.PLN;
+            model1.FirstName = "Jan";
+            model1.LastName = "Kowalski";
+            model.Add(model1);
+            return View(model);
+        }
+
+        // GET: ReportController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReportController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReportController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReportController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReportController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReportController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReportController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
